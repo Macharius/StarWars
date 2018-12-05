4cb0059d3bccb3cee40b77e2aeb3f62869dd7dc6
remipradal <remi.pradal@gmail.com>
2018-12-05 10:07:39 +0100

    First commit

---
+++ app/src/main/java/com/remipradal/starwars/TripListActivity.kt
@@ -0,0 +1,12 @@
+package com.remipradal.starwars
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TripListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trip_list)
+    }
+}
